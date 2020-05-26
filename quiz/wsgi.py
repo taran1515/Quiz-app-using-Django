aa52eef8fdcabcd6aa68c1189982018d55182a7c
Taranjeet Singh <singhtaran1515@gmail.com>
2020-05-27 03:01:32 +0530

    changed setting

--- quiz/wsgi.py
+++ quiz/wsgi.py
@@ -12,8 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz.settings')
-# application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = get_wsgi_application(DjangoWhiteNoise(application))
-
+application = get_wsgi_application()
